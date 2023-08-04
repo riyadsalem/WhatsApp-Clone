6ad42c4d06de4fdd1f1f6664b93afb95c704f202
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-08-04 11:05:04 +0300

    Add some middlwares (Functionality, Security and Speed Release #1

--- WhatsApp_BackEnd/src/app.js
+++ WhatsApp_BackEnd/src/app.js
@@ -1,14 +1,39 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import cookieparser from "cookie-parser";
+import compression from "compression";
+import fileUpload from "express-fileupload";
+import cors from "cors";
 
-// dotEnv config
 dotenv.config();
-
-// create express app
 const app = express();
 
-app.get("/test", (req, res) => {
-  res.send("First Route");
+// Middlewares
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(mongoSanitize());
+app.use(cookieparser());
+app.use(compression());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+app.post("/test", (req, res) => {
+  res.send(req.body);
 });
 
 export default app;
