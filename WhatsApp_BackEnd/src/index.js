9ecf25e73a250a7e7c7a3ebf21145c53768d5c1d
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-08-04 16:42:12 +0300

    Add Custom logger whith winston & handling uncaughException, unhandledRejection errors and SIGTERM signal Closing Release #1

--- WhatsApp_BackEnd/src/index.js
+++ WhatsApp_BackEnd/src/index.js
@@ -1,8 +1,37 @@
 import app from "./app.js";
+import logger from "./configs/logger.config..js";
 
-// env variables
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log("SERVER IS LISTENING AT", `${PORT}....`);
+let server;
+server = app.listen(PORT, () => {
+  logger.info(`SERVER IS LISTENING AT ${PORT}....`);
+  // throw new Error("ERROR IS SERVER");
+  // console.log("process id", process.pid);
+});
+
+//handle server errors
+const exitHandler = () => {
+  if (server) {
+    logger.info("Server closed.");
+    process.exit(1);
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+//SIGTERM
+process.on("SIGTERM", () => {
+  if (server) {
+    logger.info("Server closed.");
+    process.exit(1);
+  }
 });
