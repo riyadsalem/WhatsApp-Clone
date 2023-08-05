bf6ac8803405b6eb770481bfcffffdc27087bf77
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-08-05 18:56:31 +0300

    Setup wailwind css and dark mode custom classess etc... Release #1

--- whatsapp_frontend/src/App.js
+++ whatsapp_frontend/src/App.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 function App() {
+  const [isDark, setIsDark] = useState(false);
   return (
-    <div className="App">
-      <h1>This is Firnst Component</h1>
+    <div className={isDark ? "dark" : "App"}>
+      <h1 className="customh1 dark:bg-dark_bg_1">
+        <p className="text-black dark:text-white">Welcome to frontend</p>
+      </h1>
+      <button
+        className="bg-blue-300 p-3 rounded-md"
+        onClick={() => setIsDark((prev) => !prev)}
+      >
+        Switch For {isDark ? "LIGHT" : "DARK"}
+      </button>
     </div>
   );
 }
