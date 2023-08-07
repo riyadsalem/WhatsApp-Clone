fcabf93efa3d4da43f10535838d759380ae0ad9a
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-08-07 06:55:18 +0300

    Setup the register and login page and it's form components Release #3

---
+++ whatsapp_frontend/src/components/auth/LoginForm.jsx
@@ -0,0 +1,14 @@
+export default function LoginFrom() {
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center overflow-hidden">
+      {/* Container */}
+      <div className="w-full max-w-md space-y-8 p-10 dark:bg-dark_bg_2 rounded-xl">
+        {/* Heading */}
+        <div className="text-center dark:text-dark_text_1">
+          <h2 className="mt-6 text-3xl font-bold">Welcome</h2>
+          <p className="mt-2 text-sm">Sign in</p>
+        </div>
+      </div>
+    </div>
+  );
+}
