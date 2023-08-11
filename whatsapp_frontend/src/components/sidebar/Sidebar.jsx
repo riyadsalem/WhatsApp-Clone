dae7c0b4af981ca71677b10ae6b399e6c08b5440
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-08-11 19:03:11 +0300

    Sidebar notifications Release #7

--- whatsapp_frontend/src/components/sidebar/Sidebar.jsx
+++ whatsapp_frontend/src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,13 @@
 import SidebarHeader from "./header/SidebarHeader";
+import Notifications from "./notifications";
 
 export default function Sidebar() {
   return (
     <div className="flex0030 max-w-[30%] h-full select-none">
       {/*Sidebar Header*/}
       <SidebarHeader />
+      {/*Notifications */}
+      <Notifications />
     </div>
   );
 }
