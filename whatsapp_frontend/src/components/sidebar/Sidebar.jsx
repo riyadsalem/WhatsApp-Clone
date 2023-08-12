152bc4e41466f5ee105759a8c5341ef1993d2421
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-08-12 09:55:17 +0300

    Sidebar search component Release #7

--- whatsapp_frontend/src/components/sidebar/Sidebar.jsx
+++ whatsapp_frontend/src/components/sidebar/Sidebar.jsx
@@ -1,13 +1,22 @@
 import SidebarHeader from "./header/SidebarHeader";
 import Notifications from "./notifications";
+import { Search } from "./search";
+import { useState } from "react";
 
 export default function Sidebar() {
+  const [searchResults, setSearchResults] = useState([]);
+
   return (
     <div className="flex0030 max-w-[30%] h-full select-none">
       {/*Sidebar Header*/}
       <SidebarHeader />
       {/*Notifications */}
       <Notifications />
+      {/* Search */}
+      <Search
+        searchLength={searchResults.length}
+        setSearchResults={setSearchResults}
+      />
     </div>
   );
 }
