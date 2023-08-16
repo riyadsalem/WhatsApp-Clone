4b84b6fe04bc0de328e7d37aa8aae8ddac290eb6
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-08-16 09:14:53 +0300

    Whatsapp main component Release #9

---
+++ whatsapp_frontend/src/components/Chat/index.js
@@ -0,0 +1,1 @@
+export { default as WhatsappHome } from "./Welcome/WhatsappHome";
--- whatsapp_frontend/src/pages/home.jsx
+++ whatsapp_frontend/src/pages/home.jsx
@@ -2,10 +2,12 @@ import Sidebar from "../components/sidebar/Sidebar";
 import { useSelector, useDispatch } from "react-redux";
 import { getConversations } from "../features/chatSlice";
 import { useEffect } from "react";
+import { WhatsappHome } from "../components/Chat";
 
 export default function Home() {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.user);
+  const { activeConversation } = useSelector((state) => state.chat);
 
   //Get Conversations
   useEffect(() => {
@@ -19,6 +21,7 @@ export default function Home() {
       {/*container*/}
       <div className="container h-screen flex py-[19px]">
         <Sidebar />
+        {activeConversation._id ? "Home" : <WhatsappHome />}
       </div>
     </div>
   );
