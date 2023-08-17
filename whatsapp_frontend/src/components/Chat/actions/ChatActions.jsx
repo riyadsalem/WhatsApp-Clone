b74f563f213c530c46fe7ef4f1c06cf3d92cb612
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-08-17 14:40:23 +0300

    Send a message Release #9

--- whatsapp_frontend/src/components/Chat/actions/ChatActions.jsx
+++ whatsapp_frontend/src/components/Chat/actions/ChatActions.jsx
@@ -1,9 +1,36 @@
 import { Attachments, EmojiPickerApp, Input } from "./index";
 import { SendIcon } from "../../../svg";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { sendMessage } from "../../../features/chatSlice";
+import { ClipLoader } from "react-spinners";
 
 export default function ChatActions() {
+  const [message, setMessage] = useState("");
+  const dispatch = useDispatch();
+
+  const { activeConversation, status } = useSelector((state) => state.chat);
+  const { user } = useSelector((state) => state.user);
+  const { token } = user;
+
+  const values = {
+    message,
+    convo_id: activeConversation._id,
+    files: [],
+    token,
+  };
+
+  const SendMessageHandler = (e) => {
+    e.preventDefault();
+    dispatch(sendMessage(values));
+    setMessage("");
+  };
+
   return (
-    <form className="dark:bg-dark_bg_2 h-[60px] w-full flex items-center absolute bottom-0 py-2 px-4 select-none">
+    <form
+      onSubmit={(e) => SendMessageHandler(e)}
+      className="dark:bg-dark_bg_2 h-[60px] w-full flex items-center absolute bottom-0 py-2 px-4 select-none"
+    >
       {/* Container */}
       <div className="w-full flex items-center gap-x-2">
         {/*Emojis and attachpments*/}
@@ -12,10 +39,14 @@ export default function ChatActions() {
           <Attachments />
         </ul>
         {/* Input */}
-        <Input />
+        <Input message={message} setMessage={setMessage} />
         {/* Send button */}
         <button type="submit" className="btn">
-          <SendIcon className="dark:fill-dark_svg_1" />
+          {status === "loading" ? (
+            <ClipLoader color="#E9EDEF" size={25} />
+          ) : (
+            <SendIcon className="dark:fill-dark_svg_1" />
+          )}
         </button>
       </div>
     </form>
--- whatsapp_frontend/src/features/chatSlice.js
+++ whatsapp_frontend/src/features/chatSlice.js
@@ -69,6 +69,31 @@ export const getConversationMessages = createAsyncThunk(
   }
 );
 
+export const sendMessage = createAsyncThunk(
+  "message/send",
+  async (values, { rejectWithValue }) => {
+    const { token, message, convo_id, files } = values;
+    try {
+      const { data } = await axios.post(
+        MESSAGE_ENDPOINT,
+        {
+          message,
+          convo_id,
+          files,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.response.data.error.message);
+    }
+  }
+);
+
 export const chatSlice = createSlice({
   name: "chat",
   initialState,
@@ -110,6 +135,17 @@ export const chatSlice = createSlice({
         state.messages = action.payload;
       })
       .addCase(getConversationMessages.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+      })
+      .addCase(sendMessage.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(sendMessage.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.messages = [...state.messages, action.payload];
+      })
+      .addCase(sendMessage.rejected, (state, action) => {
         state.status = "failed";
         state.error = action.payload;
       });
