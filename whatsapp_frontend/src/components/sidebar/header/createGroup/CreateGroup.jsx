a50491d85b91beb87a13c04f28bb8058cff888f1
Riyad M Salem <riyad.m.salem.19988@gmail.com>
2023-08-27 09:56:36 +0300

    LETS START (Create group) Release #18

---
+++ whatsapp_frontend/src/components/sidebar/header/createGroup/CreateGroup.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { ReturnIcon } from "../../../../svg";
+import { UnderlineInput } from "./";
+
+export default function CreateGroup({ setShowCreateGroup }) {
+  const [name, setName] = useState("");
+
+  return (
+    <div className="createGroupAnimation relative flex0030 h-full z-40">
+      {/* Container */}
+      <div className="mt-5">
+        {/* Return/Close button */}
+        <button
+          className="btn w-6 h-6 border"
+          onClick={() => setShowCreateGroup(false)}
+        >
+          <ReturnIcon className="fill-white" />
+        </button>
+        {/* Group name input */}
+        <UnderlineInput name={name} setName={setName} />
+      </div>
+    </div>
+  );
+}
